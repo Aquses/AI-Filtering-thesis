80cb70087f2fe3940c2c7766f7d2ff5dddae6b3a
Eldaras Zutautas <xyaroo@gmail.com>
2025-04-08 19:07:39 +0200

    Moved categories to analyzeHelpers

--- src/background/analyzeHelpers.js
+++ src/background/analyzeHelpers.js
@@ -1,7 +1,29 @@
 export function extractContentScore(data) {
+    // all the categories, will have to add more later.
+    const {
+        nudity = {},
+        offensive = {},
+        gore = {},
+        violence = {},
+        selfharm = {},
+        recreational_drug = {},
+        alcohol = {},
+        weapon = { classes: {} }
+    } = data;
 
+    return {
+        sexual_activity: nudity.sexual_activity || 0,
+        erotica: nudity.erotica || 0,
+        firearm: weapon.classes.firearm || 0,
+        alcohol: alcohol.prob || 0,
+        drugs: recreational_drug.prob || 0,
+        offensive: offensive.prob || 0,
+        gore: gore.prob || 0,
+        violence: violence.prob || 0,
+        selfharm: selfharm.prob || 0
+    }
 }
 
 export function isExplicitContent(scores, thresholds = {}) {
-    
+
 }
--- src/background/background.js
+++ src/background/background.js
@@ -95,42 +95,6 @@ function analyzeImageWithSightEngine(imageUrl) {
                 console.error('Invalid API response:', data);
                 return;
             }
-
-            // all the categories, will have to add more later.
-            const {
-                nudity = {},
-                offensive = {},
-                gore = {},
-                violence = {},
-                selfharm = {},
-                recreational_drug = {},
-                alcohol = {},
-                weapon = { classes: {} }
-            } = data;
-
-            console.log('Extracted Scores:', {
-                sexual_activity: nudity.sexual_activity || 0,
-                erotica: nudity.erotica || 0,
-                firearm: weapon.classes.firearm || 0,
-                alcohol: alcohol.prob || 0,
-                drugs: recreational_drug.prob || 0,
-                offensive: offensive.prob || 0,
-                gore: gore.prob || 0,
-                violence: violence.prob || 0,
-                selfharm: selfharm.prob || 0
-            });
-
-            const isExplicit = (
-                (nudity.sexual_activity || 0) > 0.95 ||
-                (nudity.erotica || 0) > 0.95 ||
-                (weapon.classes.firearm || 0) > 0.95 ||
-                (alcohol.prob || 0) > 0.95 ||
-                (recreational_drug.prob || 0) > 0.95 ||
-                (offensive.prob || 0) > 0.95 ||
-                (gore.prob || 0) > 0.95 ||
-                (violence.prob || 0) > 0.95 ||
-                (selfharm.prob || 0) > 0.95
-            );
             
             console.log('Explicit content detected?', isExplicit);
 
