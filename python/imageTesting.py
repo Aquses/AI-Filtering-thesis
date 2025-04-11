5b586fcfbcc0ac396b779e2a19fede2bcfb70fcd
Eldaras Zutautas <ez222eq@student.lnu.se>
2025-04-11 16:24:47 +0200

    variable to get all images from folder

--- python/imageTesting.py
+++ python/imageTesting.py
@@ -9,12 +9,17 @@ api_key = os.getenv('SIGHT_ENGINE_API_USER')
 api_secret = os.getenv('SIGHT_ENGINE_API_SECRET')   
 
 params = {
-  'models': 'weapon',
+  'models': 'nudity-2.1,weapon,alcohol,recreational_drug,medical,offensive-2.0,gore-2.0,violence,self-harm',
   'api_user': api_key,
   'api_secret': api_secret
 }
-files = {'media': open('../python/test_images/Glock_17.jpg', 'rb')}
-r = requests.post('https://api.sightengine.com/1.0/check.json', files=files, data=params)
+
+image_dir = '../python/test_images/'
+
+files = [img for img in os.listdir(image_dir) if img.lower().endswith(('.jpg', '.png', '.jpeg'))]
+
+# files = {'media': open('../python/test_images/Nprofile2.jpg', 'rb')}
+# r = requests.post('https://api.sightengine.com/1.0/check.json', files=files, data=params)
 
 output = json.loads(r.text)
 print(output)
