2fa9a6880d6344bef84a996c2853a320718aff3d
Eldaras Zutautas <ez222eq@student.lnu.se>
2025-04-11 16:14:39 +0200

    Manually testing one image works

--- python/imageTesting.py
+++ python/imageTesting.py
@@ -1,13 +1,21 @@
-import unitest
 from dotenv import load_dotenv
-from sightengine import Client
-from PIL import Image
+import requests
+import json
 import os
-
 
 load_dotenv()
 
 api_key = os.getenv('SIGHT_ENGINE_API_USER')
-api_secret = os.getenv('SIGHT_ENGINE_API_SECRET')
+api_secret = os.getenv('SIGHT_ENGINE_API_SECRET')   
 
-client = Client(api_key, api_secret)
+params = {
+  'models': 'weapon',
+  'api_user': api_key,
+  'api_secret': api_secret
+}
+files = {'media': open('../python/test_images/Glock_17.jpg', 'rb')}
+r = requests.post('https://api.sightengine.com/1.0/check.json', files=files, data=params)
+
+output = json.loads(r.text)
+print(output)
+
